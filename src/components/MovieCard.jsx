84c026a879fdb756c14487f41ca7a96901869257
aevangemert <anna.e.gemert@hiof.no>
2026-03-11 13:18:48 +0100

    fikset img-tagen til å rendre ut bildre

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ export default function MovieCard({Title, Year, Poster}){
     return(
         <article>
             <h2>{Title}</h2>
-            <img src={Poster.image} alt={Poster} />
+            <img src={Poster} alt={Poster} />
             <p>{Year}</p>
         </article>
     )
